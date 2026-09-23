e54902873556644fa7c599ae1e71d12d51c596aa
Kaushik <contact.kaushikraj.cse@gmail.com>
2026-09-23 17:53:52 +0530

    Connect frontend to deployed backend

--- src/components/AICompanion.jsx
+++ src/components/AICompanion.jsx
@@ -10,7 +10,9 @@ import { calculateWellnessScore } from "../utils/wellnessScore";
 
 import "../App.css";
 
-const API_URL = "http://127.0.0.1:8000";
+const API_URL =
+  import.meta.env.VITE_API_URL ||
+  "http://127.0.0.1:8000";
 
 
 /* =========================================================
